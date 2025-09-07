ebd7c72dc46cce8a7714da0518239e3f4224376e
marsellinuspaska <marsellinuspaska@gmail.com>
2025-09-07 14:16:01 +0700

    update-bottom-navbar

--- mpo_template/assets/js/main.js
+++ mpo_template/assets/js/main.js
@@ -54,7 +54,28 @@
             document.querySelectorAll('.offCanvas__overly').forEach(function(el){ el.classList.add('active'); });
         });
     }
-    
+
+    /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+    const sections = document.querySelectorAll('section[id]')
+
+    function scrollActive(){
+        const scrollY = window.pageYOffset
+
+        sections.forEach(current =>{
+            const sectionHeight = current.offsetHeight,
+                sectionTop = current.offsetTop - 50,
+                sectionId = current.getAttribute('id')
+
+            if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+            }else{
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+            }
+        })
+    }
+    window.addEventListener('scroll', scrollActive)
+
+
     // SWIPER PAYMENT METHOD ///
             document.addEventListener('DOMContentLoaded', function () {
                 new Swiper('.slider-payment-method', {
